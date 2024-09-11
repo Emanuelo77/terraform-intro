5386f17df2848a4ce9bc3e1b8e54f1e05033b617
Emanuel crisan <emanuelcrisan778@gmail.com>
2024-09-11 18:16:22 +0200

    122

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -2,157 +2,195 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+
 # VPC
 resource "aws_vpc" "main_vpc_prod" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
-        Name = "main-prod-vpc"
-    }
+
+  tags = {
+    Name = "main_prod_vpc"
+  }
 }
+
 
 # Internet Gateway
 resource "aws_internet_gateway" "main_igw_prod" {
-    vpc_id = aws_vpc.main_vpc_prod.id
+  vpc_id = aws_vpc.main_vpc_prod.id
 
-    tags = {
-        Name = "main-prod-igw"
-    }
-  
+
+  tags = {
+    Name = "main_prod_igw"
+  }
 }
 
-# Public subnet
+
+# Public Subnet A
 resource "aws_subnet" "main_public_subnet_a_prod" {
-    vpc_id = aws_vpc.main_vpc_prod.id
-    cidr_block = "10.0.0.0/20"
-    availability_zone = "eu-central-1a"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main_vpc_prod.id
+  cidr_block              = "10.0.0.0/20"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
 
-    tags = {
-        Name = "main-prod-public-subnet-a"
-    }
-  
+
+  tags = {
+    Name = "main_prod_public_subnet_a"
+  }
 }
 
-# Private subnet
+
+# Private Subnet A
 resource "aws_subnet" "main_private_subnet_a_prod" {
-    vpc_id = aws_vpc.main_vpc_prod.id
-    cidr_block = "10.0.128.0/20"
-    availability_zone = "eu-central-1a"
+  vpc_id            = aws_vpc.main_vpc_prod.id
+  cidr_block        = "10.0.128.0/20"
+  availability_zone = "eu-central-1a"
 
-    tags = {
-        Name = "main-prod-private-subnet-a"
-    }
-  
+
+  tags = {
+    Name = "main_prod_private_subnet_a"
+  }
 }
 
-# Public subnet
-resource "aws_subnet" "main_public_subnet_b_prod" {
-    vpc_id = aws_vpc.main_vpc_prod.id
-    cidr_block = "10.0.16.0/20"
-    availability_zone = "eu-central-1b"
-    map_public_ip_on_launch = true
-
-    tags = {
-        Name = "main-prod-public-subnet-b"
-    }
-  
-}
-
-# Private subnet
-resource "aws_subnet" "main_private_subnet_b_prod" {
-    vpc_id = aws_vpc.main_vpc_prod.id
-    cidr_block = "10.0.144.0/20"
-    availability_zone = "eu-central-1b"
-
-    tags = {
-        Name = "main-prod-private-subnet-b"
-    }
-  
-}
 
 # Public Route Table
 resource "aws_route_table" "public_rtb_prod" {
-    vpc_id = aws_vpc.main_vpc_prod.id
+  vpc_id = aws_vpc.main_vpc_prod.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main_igw_prod.id
-    }
 
-    tags = {
-        Name = "main-prod-vpc-public-route-table"
-    }
-  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw_prod.id
+  }
+
+
+  tags = {
+    Name = "main_prod_vpc_public_route_table"
+  }
 }
 
-# Public Subnet to Public Route Table Association
-resource "aws_route_table_association" "public_rtb_subnet_assoc_prod-a" {
-    subnet_id = aws_subnet.main_public_subnet_a_prod.id
-    route_table_id = aws_route_table.public_rtb_prod.id
+
+# Public Subnet to Public Route Table Association A
+resource "aws_route_table_association" "public_rtb_subnet_assoc_prod_a" {
+  subnet_id      = aws_subnet.main_public_subnet_a_prod.id
+  route_table_id = aws_route_table.public_rtb_prod.id
 }
 
-resource "aws_route_table_association" "public_rtb_subnet_assoc_prod-b" {
-    subnet_id = aws_subnet.main_public_subnet_b_prod.id
-    route_table_id = aws_route_table.public_rtb_prod.id
+
+# Public Subnet B
+resource "aws_subnet" "main_public_subnet_b_prod" {
+  vpc_id                  = aws_vpc.main_vpc_prod.id
+  cidr_block              = "10.0.16.0/20"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
+
+
+  tags = {
+    Name = "main_prod_public_subnet_b"
+  }
 }
+
+
+# Private Subnet B
+resource "aws_subnet" "main_private_subnet_b_prod" {
+  vpc_id            = aws_vpc.main_vpc_prod.id
+  cidr_block        = "10.0.144.0/20"
+  availability_zone = "eu-central-1b"
+
+
+  tags = {
+    Name = "main_prod_private_subnet_b"
+  }
+}
+
+
+# Public Subnet to Public Route Table Association B
+resource "aws_route_table_association" "public_rtb_subnet_assoc_prod_b" {
+  subnet_id      = aws_subnet.main_public_subnet_b_prod.id
+  route_table_id = aws_route_table.public_rtb_prod.id
+}
+
 
 # Security Group
 resource "aws_security_group" "web_sg_prod" {
-    vpc_id = aws_vpc.main_vpc_prod.id
+  vpc_id = aws_vpc.main_vpc_prod.id
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
 
-    tags = {
-        Name = "web-security-group-prod"
-    }
-  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "web_security_group_prod"
+  }
 }
 
-# EC2 Instance - Web Server
-resource "aws_instance" "web_server_prod" {
-    ami = "ami-0de02246788e4a354"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.main_public_subnet_a_prod.id
-    vpc_security_group_ids = [ aws_security_group.web_sg_prod.id ]
 
-    user_data = <<-EOF
-                #!/bin/bash
-                dnf update -y
-                dnf install -y httpd
-                systemctl start httpd
-                systemctl enable httpd
-                echo "Hello World from $(hostname -f)!" > /var/www/html/index.html
-                EOF
+# EC2 Instance - Feedback-App Server
+resource "aws_instance" "feedback_app_server_prod" {
+  ami                    = "ami-0de02246788e4a354"  # Amazon Linux 2 AMI, Region: eu-central-1
+  instance_type          = "t2.micro"               # Kostenfreie Instanzgröße, ändere nach Bedarf
+  subnet_id              = aws_subnet.main_public_subnet_a_prod.id
+  vpc_security_group_ids = [aws_security_group.web_sg_prod.id]
 
-    tags = {
-        Name = "web-server-prod"
-    }
-  
+
+  user_data = <<-EOF
+    #!/bin/bash
+    # System Update
+    yum update -y
+    yum install -y docker
+
+
+    # Start Docker
+    service docker start
+    systemctl enable docker
+
+
+    # Docker-Compose installieren
+    curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+
+
+    # Verzeichnis für die App erstellen und die Docker-Compose Datei herunterladen
+    mkdir -p /home/ec2-user/feedback-app
+    cd /home/ec2-user/feedback-app
+    curl -L "https://raw.githubusercontent.com/Emanuelo77/feedback-app/main/docker-compose.yml" -o docker-compose.yml
+
+
+    # Feedback-App starten
+    docker-compose up -d
+  EOF
+
+
+  tags = {
+    Name = "Feedback-App-Server"
+  }
 }
+
 
 # Outputs
-
-output "instance_public_ip" {
-    description = "The public IP of the EC2 instance"
-    value = aws_instance.web_server_prod.public_ip
+output "feedback_app_instance_public_ip" {
+  description = "Die öffentliche IP der EC2-Instanz mit der Feedback-App"
+  value       = aws_instance.feedback_app_server_prod.public_ip
 }
+
+
---
+++ terraform-vpc/variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_id" {
+  description = "Die ID der VPC, in der die EC2-Instanz erstellt wird"
+  type        = string
+  default     = "vpc-0a75d01afbf38a285" # Ersetze durch deine VPC-ID
+}
+
+
+
